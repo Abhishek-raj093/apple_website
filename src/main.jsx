da304656ee3781dd15d4bb75e8cd85d3dbec92e2
Abhishek-raj093 <ss0986825@gmail.com>
2025-01-20 00:02:53 +0530

    Final Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://cd734d1168b114238ae1262480ccc253@o4508584727543808.ingest.us.sentry.io/4508668724051968",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    //Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
